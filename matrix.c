6d4b9b3a2ea5de3c2d9b675329ffb0e6fbe341cb
Ryazanov Alex <razanovaleksej153@gmail.com>
2024-07-12 16:55:20 +0300

    Добавлен файл matrix.c.

---
+++ matrix.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+int matrixgrafh[MAX][MAX];
+size_t rows;
+size_t cols;
+} matrix;
+
+int main (void)
+{
+size_t j, k;
+matrix m;
+
+if (LoadMatrix (&m) == -1) {
+    fprintf (stderr, "error: LoadMatrix failed.\n");
+    return 1;
+}
+
+printf ("\nThe %zux%zu matrix read from file is:\n\n", m.rows, m.cols);    
+for (j = 0; j < m.rows; j++) {
+    char *pad = " [ "; \
+    for (k = 0; k < m.cols; k++) {
+        printf ("%s%2d", pad, m.matrixgrafh[j][k]);
+        pad = ", ";
+    }
+    printf (" ]\n");
+}
+putchar ('\n');
+
+return 0;
+}
